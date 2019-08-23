b0c04013e86a686e8d1dc7bde296d133d64a3adf
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-08-23 18:10:51 +0200

    Move signal type conversion functions to sys module
    From the unix module.

--- src/sys/unix.rs
+++ src/sys/unix.rs
@@ -1,4 +1,4 @@
-use crate::Signal;
+use super::{from_raw_signal, raw_signal};
 
 #[cfg(any(target_os = "linux", target_os = "android"))]
 mod signalfd {
@@ -351,61 +351,3 @@ mod kqueue {
     target_os = "openbsd"
 ))]
 pub use self::kqueue::Signals;
-
-/// Convert a `signal` into a Unix signal.
-fn raw_signal(signal: Signal) -> libc::c_int {
-    match signal {
-        Signal::Interrupt => libc::SIGINT,
-        Signal::Quit => libc::SIGQUIT,
-        Signal::Terminate => libc::SIGTERM,
-    }
-}
-/// Convert a raw Unix signal into a signal.
-fn from_raw_signal(raw_signal: libc::c_int) -> Option<Signal> {
-    match raw_signal {
-        libc::SIGINT => Some(Signal::Interrupt),
-        libc::SIGQUIT => Some(Signal::Quit),
-        libc::SIGTERM => Some(Signal::Terminate),
-        _ => None,
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::Signal;
-
-    use super::{from_raw_signal, raw_signal};
-
-    #[test]
-    fn test_from_raw_signal() {
-        assert_eq!(from_raw_signal(libc::SIGINT), Some(Signal::Interrupt));
-        assert_eq!(from_raw_signal(libc::SIGQUIT), Some(Signal::Quit));
-        assert_eq!(from_raw_signal(libc::SIGTERM), Some(Signal::Terminate));
-
-        // Unsupported signals.
-        assert_eq!(from_raw_signal(libc::SIGSTOP), None);
-    }
-
-    #[test]
-    fn test_raw_signal() {
-        assert_eq!(raw_signal(Signal::Interrupt), libc::SIGINT);
-        assert_eq!(raw_signal(Signal::Quit), libc::SIGQUIT);
-        assert_eq!(raw_signal(Signal::Terminate), libc::SIGTERM);
-    }
-
-    #[test]
-    fn raw_signal_round_trip() {
-        assert_eq!(
-            raw_signal(from_raw_signal(libc::SIGINT).unwrap()),
-            libc::SIGINT
-        );
-        assert_eq!(
-            raw_signal(from_raw_signal(libc::SIGQUIT).unwrap()),
-            libc::SIGQUIT
-        );
-        assert_eq!(
-            raw_signal(from_raw_signal(libc::SIGTERM).unwrap()),
-            libc::SIGTERM
-        );
-    }
-}
